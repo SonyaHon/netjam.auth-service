c9ecab2cdc63e5dee0fa084a23cef50e37a0e0de
pvolkov <pvolkov@delimobil.ru>
2020-08-24 13:02:07 +0300

    add get repository

--- src/providers/database.provider.ts
+++ src/providers/database.provider.ts
@@ -1,11 +1,22 @@
 import { ProviderBase, Init } from "@netjam/server";
-import { createConnection, Connection } from "typeorm";
+import { createConnection, Connection, Repository } from "typeorm";
+import { User } from "../entity/user.entity";
 
 export class DatabaseProvider extends ProviderBase {
   private connection: Connection;
+  private userRepository: Repository<User>;
 
   @Init
   async init() {
     this.connection = await createConnection();
+    this.userRepository = this.connection.getRepository(User);
+  }
+
+  getRepository(repository: "user") {
+    const map = {
+      user: this.userRepository,
+    };
+
+    return map[repository];
   }
 }
