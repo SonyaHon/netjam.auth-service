49de948c4c5f0de7c570bb97dc245a6f01a7c9d4
pvolkov <pvolkov@delimobil.ru>
2020-08-25 19:52:42 +0300

    start jwt

--- src/providers/auth.provider.ts
+++ src/providers/auth.provider.ts
@@ -5,7 +5,7 @@ import { UserProvider } from "./user.provider";
 import { User } from "../entity/user.entity";
 import { HTTP_CODE, Errorable, ERROR_CODE } from "../reference/error";
 import { compare } from "bcrypt";
-import { rejects } from "assert";
+import { sign, verify } from "jsonwebtoken";
 
 export interface IAuthLogin {
   username: string;
@@ -25,22 +25,26 @@ export class AuthProvider extends ProviderBase {
 
   @Post("/login")
   async login(@Body() body: IAuthLogin, @Res() response: Response): Promise<Errorable<any>> {
+    const ERR = {
+      code: ERROR_CODE.USER_NOT_FOUND,
+      message: "Username or password is incorrect",
+    };
+
     const user = await this.getProvider<UserProvider>(UserProvider.name).getUser(body.username);
     if (!user) {
       response.status(HTTP_CODE.NOT_FOUND);
-      return {
-        code: ERROR_CODE.USER_NOT_FOUND,
-        message: "Username or password is incorrect",
-      };
+      return ERR;
     }
     let unwrapedUser = user as User;
     const res = await compare(body.password, unwrapedUser.password);
-    if (res) {
-      // login user
-      return true;
+    if (!res) {
+      response.status(HTTP_CODE.NOT_FOUND);
+      return ERR;
     }
-    else {
-      return false;
-    }
+
+    const token = sign({ userId: unwrapedUser.id }, process.env.NJ_JWT_SECRET || "secret");
+    console.log("Enc", token);
+    console.log("Dec", verify(token, "secret"));
+    return true;
   }
 }
